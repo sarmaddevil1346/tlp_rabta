449606e292b2eb274a62793d6e5a7972b4f6399a
Muhammad Sarmad Nadeem <sarmadofficial1366@gmail.com>
2024-01-29 09:23:04 +0500

    do some changes in home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,103 +12,122 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Container(
-                  height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                    border: Border.all(
-                      color: Colors.red,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      border: Border.all(
+                        color: Colors.red,
+                      ),
                     ),
+                    child: const Icon(Icons.person_2_outlined),
                   ),
-                  child: const Icon(Icons.person_2_outlined),
-                ),
-                const Column(
+                  SizedBox(
+                    width: 10,
+                  ),
+                  const Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Welcome"),
+                      Text("Saadi"),
+                    ],
+                  ),
+                  const Spacer(),
+                  Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(40),
+                        border: Border.all(
+                          color: Colors.black,
+                        )),
+                    child: const Icon(Icons.notifications_outlined),
+                  ),
+                ],
+              ),
+              Spacer(),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                height: 70,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.all(Radius.circular(20))),
+                child: const Row(
                   children: [
-                    Text("Welcome"),
-                    Text("Saadi"),
+                    Text(
+                      "TLP Candidate Symbol Search",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    Spacer(),
+                    Icon(Icons.arrow_forward_ios_outlined)
                   ],
                 ),
-                const Spacer(),
-                Container(
-                  height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(40),
-                      border: Border.all(
-                        color: Colors.black,
-                      )),
-                  child: const Icon(Icons.notifications_outlined),
-                ),
-              ],
-            ),
-            Container(
-              height: 70,
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                  color: Colors.green,
-                  borderRadius: BorderRadius.all(Radius.circular(20))),
-              child: const Row(
-                children: [
-                  Text(
-                    "TLP Candidate Symbol Search",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  Spacer(),
-                  Icon(Icons.arrow_forward_ios_outlined)
-                ],
               ),
-            ),
-            Container(
-              height: 70,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black),
-                  borderRadius: const BorderRadius.all(Radius.circular(20))),
-              child: const Row(
-                children: [
-                  Text(
-                    "TLP Candidate Symbol Search",
-                    style: TextStyle(color: Colors.black),
-                  ),
-                  Spacer(),
-                  Icon(Icons.arrow_forward_ios_outlined)
-                ],
+              SizedBox(
+                height: 10,
               ),
-            ),
-            Spacer(),
-            Expanded(
-              child: GridView.builder(
-                itemCount: 6,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                  crossAxisSpacing: 15,
-                  mainAxisSpacing: 15,
-                ),
-                itemBuilder: (context, index) {
-                  return Container(
-                    height: 100,
-                    width: 100,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        border: Border.all(color: Colors.black)),
-                    child: const Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(Icons.person),
-                        Text("Profile Status"),
-                        Text("17%")
-                      ],
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                height: 70,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black),
+                    borderRadius: const BorderRadius.all(Radius.circular(20))),
+                child: const Row(
+                  children: [
+                    Text(
+                      "Latest Party News and Announcements",
+                      style: TextStyle(color: Colors.black),
                     ),
-                  );
-                },
+                    Spacer(),
+                    Icon(Icons.arrow_forward_ios_outlined)
+                  ],
+                ),
               ),
-            )
-          ],
+              SizedBox(
+                height: 10,
+              ),
+              Expanded(
+                child: GridView.builder(
+                  itemCount: 6,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 15,
+                    mainAxisSpacing: 15,
+                  ),
+                  itemBuilder: (context, index) {
+                    return Container(
+                      height: 100,
+                      width: 100,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          border: Border.all(color: Colors.black)),
+                      child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.person),
+                          Text("Profile Status"),
+                          Text("17%")
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
