acd59aca041553b11ced739a4d4a0d7fe9af021c
Muhammad Sarmad Nadeem <sarmadofficial1366@gmail.com>
2024-01-29 09:36:25 +0500

    do some changes in home screen and in UI

--- lib/screens/forget_screen.dart
+++ lib/screens/forget_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:tlp_rabta/screens/home_screen.dart';
-import 'package:tlp_rabta/screens/search_category.dart';
-import 'package:tlp_rabta/screens/signup_screen.dart';
 import 'package:tlp_rabta/utils/default_button.dart';
 import 'package:tlp_rabta/utils/detault_text_form_fields.dart';
 
@@ -27,90 +25,92 @@ class _ForgetScreenState extends State<ForgetScreen> {
                 image: AssetImage("assets/images/background.jpg"))),
         child: Scaffold(
           backgroundColor: Colors.transparent,
-          body: Column(
-            children: [
-              const SizedBox(
-                height: 60,
-              ),
-              Center(
-                child: Container(
-                  height: 200,
-                  width: 200,
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: AssetImage("assets/images/logo.png"),
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 60,
+                ),
+                Center(
+                  child: Container(
+                    height: 200,
+                    width: 200,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: AssetImage("assets/images/logo.png"),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 60,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                child: Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  height: 500,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Column(
-                    children: [
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: Text(
-                              "CNIC / NICOP / POC",
-                              style: TextStyle(fontSize: 12),
-                            ),
-                          ),
-                          Expanded(
-                            child: Text(
-                              "سی این آئی سی / این آئی سی او پی / پی او سی",
-                              style: TextStyle(fontSize: 12),
-                            ),
-                          ),
-                        ],
-                      ),
-                      DefaultTextFields(
-                        labelText: "CNIC / NICOP / POC",
-                        borderRadius: OutlineInputBorder(
-                            borderSide: const BorderSide(color: Colors.black),
-                            borderRadius: BorderRadius.circular(40)),
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      DefaultButton(
-                        text: 'Submit',
-                        boxDecoration: BoxDecoration(
-                          color: Colors.red,
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        textStyle: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 19,
-                        ),
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const HomeScreen(),
-                            ),
-                          );
-                        },
-                      ),
-                    ],
-                  ),
+                const SizedBox(
+                  height: 60,
                 ),
-              )
-            ],
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    height: 500,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Column(
+                      children: [
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Text(
+                                "CNIC / NICOP / POC",
+                                style: TextStyle(fontSize: 12),
+                              ),
+                            ),
+                            Expanded(
+                              child: Text(
+                                "سی این آئی سی / این آئی سی او پی / پی او سی",
+                                style: TextStyle(fontSize: 12),
+                              ),
+                            ),
+                          ],
+                        ),
+                        DefaultTextFields(
+                          labelText: "CNIC / NICOP / POC",
+                          borderRadius: OutlineInputBorder(
+                              borderSide: const BorderSide(color: Colors.black),
+                              borderRadius: BorderRadius.circular(40)),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        DefaultButton(
+                          text: 'Submit',
+                          boxDecoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          textStyle: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 19,
+                          ),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const HomeScreen(),
+                              ),
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ));
   }
--- lib/screens/search_category.dart
+++ lib/screens/search_category.dart
@@ -21,94 +21,97 @@ class _SearchHalqaScreenState extends State<SearchHalqaScreen> {
       child: Scaffold(
         backgroundColor: Colors.transparent,
         body: SafeArea(
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  CircleAvatar(
-                    backgroundColor: Colors.white54,
-                    child: InkWell(
-                      onTap: () {
-                        Navigator.pop(context);
-                      },
-                      child: const Icon(
-                        Icons.arrow_back,
-                        color: Colors.white,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.white54,
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Icon(
+                          Icons.arrow_back,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
-                  ),
-                  const Text(
-                    "Tehreek-e-Labbaik Pakistan",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  const Text(
-                    "تحریک لبیک پاکستان",
-                    style: TextStyle(color: Colors.white),
-                  )
-                ],
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Expanded(
-                child: Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    height: double.infinity,
-                    width: double.infinity,
-                    decoration: const BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(20),
-                          topRight: Radius.circular(20),
-                        )),
-                    child: Stack(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                            height: 300,
-                            width: double.infinity,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                            child: Column(
-                              children: [
-                                const Text(
-                                  "Constituency / Halqa / City ",
-                                  style: TextStyle(color: Colors.white54),
-                                ),
-                                const SizedBox(
-                                  height: 20,
-                                ),
-                                DefaultTextFields(
-                                  labelText:
-                                      'Type e.g: NA-46 ,ICT , NA-18 Haripur',
-                                  borderRadius: const OutlineInputBorder(),
-                                ),
-                                const SizedBox(
-                                  height: 20,
-                                ),
-                                DefaultButton(
-                                  boxDecoration: BoxDecoration(
-                                      color: Colors.red,
-                                      borderRadius: BorderRadius.circular(20)),
-                                  text: "Search",
-                                  textStyle: const TextStyle(
-                                      fontSize: 18,
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.w600),
-                                  onTap: () {},
-                                )
-                              ],
+                    const Text(
+                      "Tehreek-e-Labbaik Pakistan",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    const Text(
+                      "تحریک لبیک پاکستان",
+                      style: TextStyle(color: Colors.white),
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Expanded(
+                  child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      height: double.infinity,
+                      width: double.infinity,
+                      decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(20),
+                            topRight: Radius.circular(20),
+                          )),
+                      child: Stack(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              height: 300,
+                              width: double.infinity,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Column(
+                                children: [
+                                  const Text(
+                                    "Constituency / Halqa / City ",
+                                    style: TextStyle(color: Colors.white54),
+                                  ),
+                                  const SizedBox(
+                                    height: 20,
+                                  ),
+                                  DefaultTextFields(
+                                    labelText:
+                                        'Type e.g: NA-46 ,ICT , NA-18 Haripur',
+                                    borderRadius: const OutlineInputBorder(),
+                                  ),
+                                  const SizedBox(
+                                    height: 20,
+                                  ),
+                                  DefaultButton(
+                                    boxDecoration: BoxDecoration(
+                                        color: Colors.red,
+                                        borderRadius:
+                                            BorderRadius.circular(20)),
+                                    text: "Search",
+                                    textStyle: const TextStyle(
+                                        fontSize: 18,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w600),
+                                    onTap: () {},
+                                  )
+                                ],
+                              ),
                             ),
                           ),
-                        ),
-                      ],
-                    )),
-              )
-            ],
+                        ],
+                      )),
+                )
+              ],
+            ),
           ),
         ),
       ),
